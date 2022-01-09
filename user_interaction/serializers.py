0e776708ea20cf47bbb9c8a7ba706e6a55d3394f
karthik <gkraja@mindkosh.com>
2022-01-09 10:39:30 +0530

    Fetch environment variables from env files

--- user_interaction/serializers.py
+++ user_interaction/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import UserInteraction
 import requests
+from environ import Env
+
+env = Env()
 
 
 class UserInteractionSerializer(serializers.ModelSerializer):
@@ -10,13 +13,13 @@ class UserInteractionSerializer(serializers.ModelSerializer):
         fields = ['user_id', 'content_id', 'is_like', 'is_read']
 
     def validate_user_id(self, user_id):
-        user_response = requests.get(f"http://localhost:8000/users/{user_id}")
+        user_response = requests.get(f"{env('USER_URL')}/users/{user_id}")
         if user_response.status_code != 200:
             raise serializers.ValidationError("invalid id")
         return user_id
 
     def validate_content_id(self, content_id):
-        content_response = requests.get(f"http://localhost:3000/books/{content_id}")
+        content_response = requests.get(f"{env('CONTENT_URL')}/books/{content_id}")
         if content_response.status_code != 200:
             raise serializers.ValidationError("invalid id")
         return content_id
--- user_interaction/views.py
+++ user_interaction/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from .serializers import UserInteractionSerializer
 from .models import UserInteraction
 from rest_framework.decorators import action
-from django.db.models import Sum
+from django.db.models import Count, Case, When
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -14,7 +14,8 @@ class UserInteractionAPIViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin
 
     @action(methods=['GET'], detail=False)
     def top_contents(self, request):
-        queryset = UserInteraction.objects.values('content_id')
-        queryset = queryset.annotate(num_likes=Sum('is_like'), num_reads=Sum('is_read'))
+        queryset = self.queryset.values('content_id')
+        queryset = queryset.annotate(num_likes=Count(Case(When(is_like=True, then=1))),
+                                     num_reads=Count(Case(When(is_read=True, then=1))))
         content_ids = queryset.order_by('-num_likes', '-num_reads').values_list('content_id', flat=True)
         return Response({'content_ids': content_ids}, status=status.HTTP_200_OK)
